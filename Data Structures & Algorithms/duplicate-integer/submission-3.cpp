04d84774b26eb055c35ba6efe9c96b689231abbf
Kenan <167587573+rONin01010101010@users.noreply.github.com>
2026-09-22 01:02:42 -0400

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.cpp
@@ -0,0 +1,15 @@
+#include <unordered_set>
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+    std::unordered_set<int> mySet; 
+
+    for(int i = 0; i < nums.size(); i++){
+        if(mySet.contains(nums[i])){
+            return true;
+        }
+        mySet.insert(nums[i]);
+    }      
+    return false;
+}
+};
